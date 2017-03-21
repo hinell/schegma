82de04a63f7a35ab82ae6b56ba59f058cc99223e
Hinell <al.neodim@gmail.com>
2017-03-22 00:13:26 +0300

    Testing added

---
+++ src/test.ts
@@ -0,0 +1,23 @@
+import {Schema, SchemaMixed, isNumber} from './stigma'
+import {ifError} from "assert";
+
+let schema = {
+      name      : 'string'
+    , phone     : 'number'
+    , desc      : ['optional','string']
+    , itemsID   : ['array',function(value,key){ return value.every(isNumber) ? void 0 : '{ '+key+': array of numbers expected! }'}]
+    , message   : 'function'
+    , test      : /^\d\d\d-\d\d\d$/g
+} as SchemaMixed<any>;
+
+let target = {
+      name      : 'James'
+    , phone     : 9998887766
+    , desc      : null // missing - validateOf() has to return no errors here
+    , itemsID   : [1,3,4,5]
+    , message   : function () { return 'HELLO!' }
+    , test      : '333-444'
+    };
+
+ifError(new Schema(schema).validateOf(target));
+console.log('All tests passed successfully');
